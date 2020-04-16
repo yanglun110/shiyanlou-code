fb31ac71f6ec8a1edaffe0f63d9da04f8964cef3
yanglun <878393391@qq.com>
2020-04-16 16:35:40 +0800

    100以内7和7的倍数跳过

---
+++ jump7.py
@@ -0,0 +1,12 @@
+for i in range(1,101):
+        if i%7 != 0 and i// 10 != 7 and i%10 != 7:
+                    print(i)
+
+#a = 1
+#while a < 101:
+#   if a % 7 == 0 or a // 10 = 7 or a%10 == 7:
+#       pass
+#   else:
+#       print(a)
+#   a+=1
+
